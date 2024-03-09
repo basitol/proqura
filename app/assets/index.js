d11fb462c39eacdd834b67c967f7e190427dd51b
basitol <basitolaitan27@gmail.com>
2024-03-09 07:19:49 +0100

    Added assets folder

---
+++ app/assets/index.js
@@ -0,0 +1,5 @@
+export {default as Logo} from './logo.svg';
+export {default as HeroImg} from './heroImg.png';
+export {default as Clipboard} from './clipboard.svg';
+export {default as Role} from './role.svg';
+export {default as Seller} from './seller.svg';
